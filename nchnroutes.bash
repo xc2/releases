afb421eb7a2a7e38c3697e62f927237a4e58cd58
xc2 <xc2@noreply.github.com>
2023-01-12 22:58:53 +0800

    cidr

--- nchnroutes.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -Lso delegated-apnic-latest https://ftp.apnic.net/stats/apnic/delegated-apnic-latest
-
-cat delegated-apnic-latest | grep -E '^apnic\|[A-Z]{2}\|ipv4\|' | grep -v '|CN|' | awk -F'|' '{print $4"/"32-log($5)/log(2)}' | aggregate -q > non-china.txt
--- netflix-aws.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo > cidr.txt
-for as in 55095 40027 394406 2906 136292; do
-  whois -h whois.radb.net -- '-i origin AS'$as | grep -Eo '([0-9.]+){4}/[0-9]+' >> cidr.txt
-done
-wget https://ip-ranges.amazonaws.com/ip-ranges.json
-jq -r '[.prefixes[] | select(.service=="AMAZON" and (.region | startswith("us-"))).ip_prefix] | .[]' < ip-ranges.json >> cidr.txt
-cat cidr.txt | grep -v '^$' | aggregate -q > netflix-aws.txt
